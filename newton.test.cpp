78014b27ce6c738aa36f0e6bce3532cbc476c299
Nicola Sebastianutto <nicola.sebastianutto@studio.unibo.it>
2023-05-30 16:04:17 +0200

    add tests

--- newton.test.cpp
+++ newton.test.cpp
@@ -35,4 +35,49 @@ TEST_CASE("Testing the class handling a floating point data sample")
     CHECK(newton.f_x == doctest::Approx(-1.75184283e-12));
     CHECK(newton.f_y == doctest::Approx(-1.75184283e-12));
   }
+
+  SUBCASE("First Universe")
+  {
+    G::Newton n{};
+    U::Universe u(n);
+    G::PlanetState p1{35, 100, 100, 0, 0};
+    G::PlanetState p2{674938, 30100, 30100, 0, 0};
+    u.push_back(p1); u.push_back(p2);
+
+
+//std::vector<G::PlanetState> uu=u.state;
+  
+
+    CHECK(u.state()[0].m == doctest::Approx(35));
+    CHECK(u.state()[1].m == doctest::Approx(674938));
+    CHECK(u.state()[0].x == doctest::Approx(100));
+    CHECK(u.state()[1].x == doctest::Approx(30100));
+    CHECK(u.state()[0].y == doctest::Approx(100));
+    CHECK(u.state()[1].y == doctest::Approx(30100));
+    CHECK(u.state()[0].v_x== doctest::Approx(0));
+    CHECK(u.state()[1].v_x == doctest::Approx(0));
+    CHECK(u.state()[0].v_y== doctest::Approx(0));
+    CHECK(u.state()[1].v_y == doctest::Approx(0));
+    
+    u.evolve(1);
+    }
+
+  SUBCASE("First Universe")
+  {
+    G::Newton n{};
+    U::Universe u(n);
+    G::PlanetState p1{5e24,5,10,20,20};
+    G::PlanetState p2{7e27,30,50,5,15};
+    u.push_back(p1); u.push_back(p2);
+
+
+//std::vector<G::PlanetState> uu=u.state;
+  
+    u.evolve(1);
+
+    CHECK(u.state()[0].v_x == doctest::Approx(35));
+   
+    
+    }
+
 }
