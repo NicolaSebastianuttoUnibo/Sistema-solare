c9d43c211997b35046c91363d677dedad0b7885b
Nicola Sebastianutto <nicola.sebastianutto@studio.unibo.it>
2023-08-13 18:24:17 +0200

    add file.txt and provvisorio.cpp

---
+++ provvisorio.cpp
@@ -0,0 +1,6 @@
+///In questo programma provvisorio bisogna trovare per un modo per:
+///verificare se un file è scritto bene (ogni riga ha 7 numeri e alla fine una parola)
+//contare i pianeti di un file
+//trovare massa, posizioni,velocità,raggio,texture per ogni pianeta di un file
+//modificare dati di un file
+///creare un file con dei pianeti da zero
