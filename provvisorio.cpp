1d2f5fc55d97ae5c2d9a0dea6ddbff6c7dca4035
Marco Silvi <marco.silvi2@studio.unibo.it>
2023-08-20 19:13:17 +0200

    finish provvisorio.cpp

--- provvisorio.cpp
+++ provvisorio.cpp
@@ -1,36 +1,24 @@
-/// In questo programma provvisorio bisogna trovare per un modo per:
-/// verificare se un file è scritto bene (ogni riga ha 6 numeri e alla fine una
-/// parola)
-// contare i pianeti di un file
-// trovare massa, posizioni,velocità,raggio,texture per ogni pianeta di un file
-// modificare dati di un file
-/// creare un file con dei pianeti da zero
-
 #include <fstream>
-#include <iomanip>
+/// #include <iomanip>
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <vector>
+
+bool onlyspaces(const std::string &s) {
+  return s.find_first_not_of(' ') == std::string::npos;
+}
 
 bool isValidLine(const std::string &line) {
+  if (line.empty() || onlyspaces(line)) {
+    return true;
+  }
+
   std::istringstream iss(line);
-  double number;  // Utilizziamo un double per i numeri scientifici
-
-  if (!(iss >> std::scientific >> number)) {
-    return false;  // Non è stato possibile estrarre un numero scientifico
-  }
-
-  int count = 1;  // Contatore inizia da 1 (primo elemento letto)
-  while (count < 6 && iss >> number) {
-    if (iss.fail()) {
-      return false;  // Non è un numero valido
-    }
-    count++;
-  }
-
-  std::string lastWord;
-  if (count == 6 && (iss >> lastWord) && iss.eof()) {
-    return true;  // La riga è valida
+  double d;
+  std::string s;
+  if (iss >> d >> d >> d >> d >> d >> d >> s && iss.eof()) {
+    return true;
   }
 
   return false;
@@ -47,7 +35,7 @@ bool isValidFile(const std::string &filename) {
   std::string line;
   while (std::getline(inputFile, line)) {
     if (!isValidLine(line)) {
-      std::cerr << "Il file contiene righe non valide: " << line << std::endl;
+      std::cerr << "Il file contiene righe non valide.\n";
       return false;
     }
   }
@@ -55,38 +43,189 @@ bool isValidFile(const std::string &filename) {
   return true;
 }
 
-int countLines(const std::string &filename) {
+int countPlanets(const std::string &filename) {
   std::ifstream inputFile(filename);
 
   if (!inputFile) {
-    std::cerr << "Impossibile aprire il file " << filename << std::endl;
+    std::cerr << "Impossibile aprire il file " << filename << "\n";
     return -1;  // Valore negativo per indicare errore
   }
 
-  int lineCount = 0;
+  int planetCount = 0;
   std::string line;
   while (std::getline(inputFile, line)) {
-    lineCount++;
+    if (!line.empty() && !onlyspaces(line)) {
+      planetCount++;
+    }  /// se la riga non è vuota
   }
 
-  return lineCount;
+  return planetCount;
+}
+
+void addPlanet(const std::string &filename) {
+  std::ofstream outputFile(filename, std::ios::app);
+
+  if (!outputFile.is_open()) {
+    std::cerr << "Impossibile aprire il file " << filename << "\n";
+  }
+
+  outputFile << "\n0 0 0 0 0 0 texture";
+  outputFile.close();
+}
+
+void addPlanet(const std::string &filename, double a, double b, double c,
+               double d, double e, double f, std::string s) {
+  std::ofstream outputFile(filename, std::ios::app);
+
+  if (!outputFile.is_open()) {
+    std::cerr << "Impossibile aprire il file " << filename << "\n";
+  }
+
+  outputFile << "\n"
+             << a << " " << b << " " << c << " " << d << " " << e << " " << f
+             << " " << s;
+  outputFile.close();
+}
+
+void removePlanet(const std::string &filename, const int &index) {
+  if (index >= countPlanets(filename)) {
+    std::cerr << "Index not valid.\n";
+    return;
+  }
+  std::ifstream inputFile(filename);
+  if (!inputFile.is_open()) {
+    std::cerr << "Impossibile aprire il file di input " << filename << "\n";
+  }
+  std::vector<std::string> lines;
+  std::string line;
+  int currentLine = -1;
+  while (std::getline(inputFile, line)) {
+    if (!line.empty() && !onlyspaces(line)) {
+      currentLine++;
+    }
+    if (currentLine != index) {
+      lines.push_back(line);
+    }
+  }
+  inputFile.close();
+
+  std::ofstream outputFile(filename);
+  if (!outputFile.is_open()) {
+    std::cerr << "Impossibile aprire il file di output" << filename << "\n";
+  }
+
+  for (const std::string &line : lines) {
+    outputFile << line << "\n";
+  }
+  outputFile.close();
+}
+
+double getMass(const std::string &filename, const int &index) {
+  if (index >= countPlanets(filename)) {
+    std::cerr << "Index not valid.\n";
+    return -1;
+  }
+
+  std::ifstream inputFile(filename);
+
+  if (!inputFile) {
+    std::cerr << "Impossibile aprire il file " << filename << "\n";
+    return -1;  // Valore negativo per indicare errore
+  }
+
+  int planetCount = 0;
+  std::string line;
+  while (std::getline(inputFile, line)) {
+    if (!line.empty() && !onlyspaces(line)) {
+      planetCount++;
+    }  /// se la riga non è vuota
+    if (planetCount == index + 1) {
+      std::istringstream iss(line);
+      double d, r;
+      std::string s;
+      iss >> r >> d >> d >> d >> d >> d >> s;
+      return r;
+    }
+  }
+
+  return -1;
+}
+
+void setMass(const std::string &filename, const int &index,
+             const double &value) {
+  if (index >= countPlanets(filename)) {
+    std::cerr << "Index not valid.\n";
+    return;
+  }
+  std::ifstream inputFile(filename);
+  if (!inputFile.is_open()) {
+    std::cerr << "Impossibile aprire il file di input " << filename << "\n";
+  }
+  std::vector<std::string> lines;
+  std::string line;
+  int currentLine = -1;
+  while (std::getline(inputFile, line)) {
+    if (!line.empty() && !onlyspaces(line)) {
+      currentLine++;
+    }
+    if (currentLine != index) {
+      lines.push_back(line);
+    } else {
+      std::stringstream iss(line);
+      double a, b, c, d, e, f;
+      std::string s;
+      iss >> a >> b >> c >> d >> e >> f >> s;
+      line = std::to_string(value) + " " + std::to_string(b) + " " +
+             std::to_string(c) + " " + std::to_string(d) + " " +
+             std::to_string(e) + " " + std::to_string(f) + " " + s;
+      lines.push_back(line);
+    }
+  }
+  inputFile.close();
+
+  std::ofstream outputFile(filename);
+  if (!outputFile.is_open()) {
+    std::cerr << "Impossibile aprire il file di output" << filename << "\n";
+  }
+
+  for (const std::string &line : lines) {
+    outputFile << line << "\n";
+  }
+  outputFile.close();
 }
 
 int main() {
   std::string filename = "file.txt";  // Sostituisci con il nome del tuo file
 
-  int lines = countLines(filename);
-  if (lines >= 0) {
-    std::cout << "Il numero di righe nel file è: " << lines << std::endl;
+  int numberplanet = countPlanets(filename);
+  if (numberplanet >= 0) {
+    std::cout << "Il numero di pianeti nel file è: " << numberplanet << "\n";
   } else {
-    std::cout << "Errore durante l'apertura del file." << std::endl;
+    std::cout << "Il file non contiene pianeti.\n";
   }
 
   if (isValidFile(filename)) {
-    std::cout << "Il file è scritto correttamente." << std::endl;
+    std::cout << "Il file è scritto correttamente."
+              << "\n";
   } else {
-    std::cout << "Il file non è scritto correttamente." << std::endl;
+    std::cout << "Il file non è scritto correttamente."
+              << "\n";
   }
 
+  setMass(filename, 1, 638);
+  std::cout << "mass=" << getMass(filename, 1) << "\n";
+
+  std::string nomefile = "file";
+  std::ifstream file(nomefile + ".txt");
+  if (file.good()) {
+    std::cerr << "Esiste già il file \n";
+  } else {
+    std::ofstream prova(nomefile + ".txt");
+    if (!prova.is_open()) {
+      std::cerr << "Impossibile creare il file \n";
+    }
+    prova.close();
+    std::cout << "File prova.txt creato con successo!\n";
+  }
   return 0;
 }
