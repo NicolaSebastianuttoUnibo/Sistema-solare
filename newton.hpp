ab0c29420c8f37f36313bcdcdc0a59e951b6eb54
Lorenzo Statello <lorenzo.statello@studio.unibo.it>
2023-08-22 10:42:45 +0200

    move struct Newton in newton.hpp and add collision system

--- newton.hpp
+++ newton.hpp
@@ -1,26 +1,38 @@
 #ifndef FORZA_G_HPP
 #define FORZA_G_HPP
 
+
+
+
+
+
 namespace G {
 
+
 struct PlanetState {
-  double m{};    // mass
-  double x{};    // pos_x 1x=1000km (espresssa in megametri 10^6 m)
-  double y{};    // pos_y
-  double v_x{};  // vel x
-  double v_y{};  // vel y
+ double m{};    // mass
+ double x{};    // pos_x 1x=1000km (espresssa in megametri 10^6 m)
+ double y{};    // pos_y
+ double v_x{};  // vel x
+ double v_y{};  // vel y
+ double r{};  // radius
+
+
+
+
+ bool operator==(const PlanetState& a)const{
+   return m==a.m&&x==a.x&&y==a.y&&v_x==a.v_x&&v_y==a.v_y&&r==a.r;
+ }
+
+
 };
 
-struct Newton {
-  // la forza è espressa in megaNewton (10^6)N
-  double f_x{};  // force x
-  double f_y{};  // force y
-  void operator()(PlanetState const& a, PlanetState const& b);
 
- private:
-  double G_{6.67430e-29};  /// costante G (MN*Mm^2/kg^2) our G = usual G*10^-18
-};
+
+
+
 
 }  // namespace G
+
 
 #endif
--- universe.hpp
+++ universe.hpp
@@ -1,22 +1,49 @@
+
 #ifndef UNIVERSE_HPP
 #define UNIVERSE_HPP
 #include <vector>
 
+
 #include "newton.hpp"
 namespace U {
-class Universe {
-  G::Newton newton_;
-  std::vector<G::PlanetState> galaxy_;
-  std::vector<G::PlanetState> copy_;
-  G::PlanetState solve(G::PlanetState const &ps, double fx, double fy,
-                       double delta_t) const;
-
- public:
-  Universe(G::Newton const &newton);
-  void push_back(G::PlanetState const &ps);
-  int size() const;
-  void evolve(double delta_t);
-  std::vector<G::PlanetState> const &state() const;
+class Universe;///forward declaration
+struct Newton {
+ // la forza è espressa in megaNewton (10^6)N
+ double f_x{};  // force x
+ double f_y{};  // force y
+// void operator()(G::PlanetState const& a, G::PlanetState const& b);
+void operator()(G::PlanetState const& a, G::PlanetState const& b);
+void operator()(Universe* u,G::PlanetState const& a, G::PlanetState const& b);
+private:
+ double G_{6.67430e-29};  /// costante G (MN*Mm^2/kg^2) our G = usual G*10^-18
 };
+
+
+
+
+class Universe {
+Newton newton_;
+ std::vector<G::PlanetState> galaxy_;
+ std::vector<G::PlanetState> copy_;
+ G::PlanetState solve(G::PlanetState const &ps, double fx, double fy,
+                      double delta_t) const;
+
+
+public:
+ Universe(Newton const &newton);
+ void push_back(G::PlanetState const &ps);
+ void remove(G::PlanetState const &ps);
+ int size() const;
+ void evolve(double delta_t);
+ std::vector<G::PlanetState> const &state() const;
+};
+
+
+
+
+
+
+
+
 }  // namespace U
 #endif
