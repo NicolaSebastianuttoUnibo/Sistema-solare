5f460c950540e5f572a66a664fe804d5e71272ff
Lorenzo Statello <lorenzo.statello@studio.unibo.it>
2023-08-24 15:07:35 +0200

    correct code optimized 2

--- universe.cpp
+++ universe.cpp
@@ -1,65 +1,91 @@
+
 #include "universe.hpp"
+
 
 #include <algorithm>
 #include <cassert>
 #include <numeric>
 
+
 U::Universe::Universe(U::Newton const &newton) : newton_{newton} {}
-int U::Universe::size() const { return galaxy_.size(); }
+int U::Universe::size() const { return importantplanet_.size(); }
+
 
 void U::Universe::push_back(G::PlanetState const &ps) { galaxy_.push_back(ps); }
 
+
 void U::Universe::remove(G::PlanetState const &ps) {
-  auto it = std::find(galaxy_.begin(), galaxy_.end(), ps);
+ auto it = std::find(galaxy_.begin(), galaxy_.end(), ps);
+  assert(it!=galaxy_.end());
+
+
   if (it != galaxy_.end()) {
-    galaxy_.erase(it);
-  }
+   galaxy_.erase(it);
+ }
 }
 void U::Universe::findimportantplanet() {
-  for (auto it = galaxy_.begin(); it < galaxy_.end(); ++it) {
-    for (auto jt = galaxy_.begin(); jt < galaxy_.end(); ++jt) {
-      double ax = newton_.G_ * (*jt).m / (((*it).x - (*jt).x) * ((*it).x - (*jt).x));
-      double ay = newton_.G_ * (*jt).m / (((*it).y - (*jt).y) * ((*it).y - (*jt).y));
-      if (ax * ax + ay * ay > 1) {
-        push_back(*it);
-        push_back(*jt);
-      }
-    }
-  }
-}
- std::vector<G::PlanetState> const &U::Universe::state() const {
-  return galaxy_;
+ for (auto it = galaxy_.begin(); it < galaxy_.end(); ++it) {
+   for (auto jt = galaxy_.begin(); jt < galaxy_.end(); ++jt) {
+
+
+     double ax = newton_.G_ * (*jt).m / (((*it).r + (*jt).r) * ((*it).r + (*jt).r));
+     double ay = newton_.G_ * (*jt).m / (((*it).r + (*jt).r) * ((*it).r + (*jt).r));
+     if (ax * ax + ay * ay > 1&&it!=jt) {
+       importantplanet_.push_back(*it);
+       importantplanet_.push_back(*jt);
+     }
+   }
  }
+}
+std::vector<G::PlanetState> const &U::Universe::state() const {
+ return galaxy_;
+}
+
 
 void U::Universe::evolve(double delta_t) {
-  copy_=galaxy_;
-  assert(copy_.size()==galaxy_.size());
-  for (auto it=copy_.begin(); it<copy_.end()-2; it+=2){
-    //if(it>=copy.end)
-    int idx=std::distance(galaxy_.begin(),it);
-    newton_(*it,*(it+1)); 
-double fx = newton_.f_x; 
-double fy = newton_.f_y; 
+ findimportantplanet();
+ copy_=galaxy_;
+ assert(copy_.size()==galaxy_.size());
+  assert(galaxy_.size()!=0);
 
-    galaxy_[idx]=solve(*(it+1),fx,fy,delta_t);
-  }
-  }
+
+ for (auto it=importantplanet_.begin(); it<importantplanet_.end(); it+=2){
+  // if(it+1<importantplanet_.end()){
+ 
+ auto jt = std::find(copy_.begin(), copy_.end(), *it);
+  assert(jt!=copy_.end());
+   int idx=std::distance(copy_.begin(),jt);
+   newton_(*it,*(it+1));
+double fx = newton_.f_x;
+double fy = newton_.f_y;
+
+
+   galaxy_[idx]=solve(*(it),fx,fy,delta_t);
+
+
+
+
+  // }
+ }
+ }
 G::PlanetState U::Universe::solve(G::PlanetState const &ps, double fx,
-                                  double fy, double delta_t) const {
-  double ax = fx / ps.m; 
-  if(ax<100) {
-    std::__throw_runtime_error("a<1");
-  }
-  double vx = ps.v_x + ax * delta_t;
-  if(vx<10) {
-    std::__throw_runtime_error("v<1");
-  }
-  double x = ps.x + ps.v_x * delta_t + 0.5 * ax * delta_t * delta_t;
+                                 double fy, double delta_t) const {
+ double ax = fx / ps.m;
 
-  double ay = fy / ps.m;
-  double vy = ps.v_y + ay * delta_t;
-  double y = ps.y + ps.v_y * delta_t + 0.5 * ay * delta_t * delta_t;
 
-  G::PlanetState r{ps.m, x, y, vx, vy};
-  return r;
+ double vx = ps.v_x + ax * delta_t;
+
+
+ double x = ps.x + ps.v_x * delta_t + 0.5 * ax * delta_t * delta_t;
+
+
+ double ay = fy / ps.m;
+ double vy = ps.v_y + ay * delta_t;
+ double y = ps.y + ps.v_y * delta_t + 0.5 * ay * delta_t * delta_t;
+
+
+ G::PlanetState r{ps.m, x, y, vx, vy};
+return r;
 }
+
+
--- universe.hpp
+++ universe.hpp
@@ -1,19 +1,23 @@
 
+
 #ifndef UNIVERSE_HPP
 #define UNIVERSE_HPP
 #include <vector>
+
+
 
 
 #include "newton.hpp"
 namespace U {
 class Universe;///forward declaration
 struct Newton {
- // la forza è espressa in megaNewton (10^6)N
- double f_x{};  // force x
- double f_y{};  // force y
+// la forza è espressa in megaNewton (10^6)N
+double f_x{};  // force x
+double f_y{};  // force y
 // void operator()(G::PlanetState const& a, G::PlanetState const& b);
 void operator()(G::PlanetState const& a, G::PlanetState const& b);
 void operator()(Universe* u,G::PlanetState const& a, G::PlanetState const& b);
+
 
 const double G_{6.67430e-29*3600*3600};  /// costante G (MN*Mm^2/kg^2) our G = usual G*10^-18
 };
@@ -21,23 +25,38 @@ const double G_{6.67430e-29*3600*3600};  /// costante G (MN*Mm^2/kg^2) our G = u
 
 
 
+
+
+
+
 class Universe {
 Newton newton_;
- std::vector<G::PlanetState> galaxy_;
- std::vector<G::PlanetState> copy_;
- G::PlanetState solve(G::PlanetState const &ps, double fx, double fy,
-                      double delta_t) const;
+std::vector<G::PlanetState> galaxy_;
+std::vector<G::PlanetState> copy_;
+G::PlanetState solve(G::PlanetState const &ps, double fx, double fy,
+                     double delta_t) const;
 std::vector<G::PlanetState> importantplanet_;
-void findimportantplanet();
+
 
 public:
- Universe(Newton const &newton);
- void push_back(G::PlanetState const &ps);
- void remove(G::PlanetState const &ps);
- int size() const;
- void evolve(double delta_t);
- std::vector<G::PlanetState> const &state() const;
+void findimportantplanet();
+
+
+Universe(Newton const &newton);
+void push_back(G::PlanetState const &ps);
+void remove(G::PlanetState const &ps);
+int size() const;
+void evolve(double delta_t);
+std::vector<G::PlanetState> const &state() const;
 };
+
+
+
+
+
+
+
+
 
 
 
@@ -48,3 +67,6 @@ public:
 
 }  // namespace U
 #endif
+
+
+
